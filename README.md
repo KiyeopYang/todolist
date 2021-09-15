a4b6c280cc629b90de21f225e23fbcf2b0565e3c
Kiyeop Yang <kiyeopyang@gmail.com>
2021-09-16 00:15:24 +0900

    prisma

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 3. nodejs
 4. npm i && npm run build
 # todolist
+
+https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases/using-prisma-migrate-typescript-postgres/
---
+++ prisma/migrations/20210915151356_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Person" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Person_pkey" PRIMARY KEY ("id")
+);
